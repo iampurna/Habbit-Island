2538238e8dc3c18cee082c766ba2e4d1eb4b7bb2
Purna Lungeli <linkwithpurna@gmail.com>
2025-12-22 12:43:34 +0545

    added theme file and theme-related export components

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,15 @@
+/// Habit Island Design System
+/// This barrel file exports all theme-related components for easy importing.
+/// Usage:
+/// import 'package:habit_island/core/theme/theme.dart';
+/// // Now you can access:
+/// // - AppTheme
+/// // - AppColors
+/// // - AppTypography
+/// // - AppDimensions
+library;
+
+export 'app_theme.dart';
+export 'app_colors.dart';
+export 'app_typography.dart';
+export 'app_dimensions.dart';
