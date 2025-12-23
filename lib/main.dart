65b419cdf77e9308e9900cad53072749340f4fb6
Purna Lungeli <linkwithpurna@gmail.com>
2025-12-23 23:19:46 +0545

    initialized hive and supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:habbit_island/data/data_sources/local/hive_database.dart';
+import 'package:habbit_island/data/data_sources/remote/supabase_client.dart';
 import 'package:habbit_island/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await HiveDatabase().init();
+  await SupabaseClientManager().init();
+
   runApp(const MyApp());
 }
 
